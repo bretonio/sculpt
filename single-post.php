5ac57126533b5220844ef96a642bed4bd729700b
Eric Bailey <ericstrattonbailey@gmail.com>
2015-07-08 21:46:48 -0400

    Moved comments.php to root

--- single-post.php
+++ single-post.php
@@ -70,7 +70,7 @@ while ( have_posts() ): the_post();
       <?php
         // If comments are open or we have at least one comment, load up the comment template.
         if ( comments_open() || get_comments_number() ) :
-          include( locate_template( "/partials/comments.php" ) );
+          comments_template();
         endif;
       ?>
     </div>
